4950b14a461fd21b71debfeabd51f091c2d829d6
morbanjunior <morbanjunior@gmail.com>
2021-12-28 11:22:16 -0400

    pizza design completed

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Featured from '../componets/Featured'
 import styles from '../styles/Home.module.css'
+import PizzaList from '../componets/PizzaList'
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <Featured/>
+        <PizzaList/>
     </div>
   )
 }
