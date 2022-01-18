38f097c6396d32a257fa85749a6a4959c3c4db73
morbanjunior <morbanjunior@gmail.com>
2022-01-18 15:37:00 -0400

    API and Redux completed

--- componets/Navbar.jsx
+++ componets/Navbar.jsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
+import { useSelector } from "react-redux";
+import Link from "next/link";
 
 const Navbar = () => {
+  const quantity = useSelector((state) => state.cart.quantity);
   return (
     <div className={styles.container}>
+      <Link href="/">
       <div className={styles.item}>
         <div className={styles.callButton}>
           <Image src="/img/telephone.png" alt="" width="32" height="32" />
@@ -13,24 +17,32 @@ const Navbar = () => {
           <div className={styles.text}>012 345 678</div>
         </div>
       </div>
+      </Link>
       <div className={styles.item}>
         <ul className={styles.list}>
+        <Link href="/">
           <li className={styles.listItem}>Homepage</li>
+          </Link>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
+          <Link href="/">
           <Image src="/img/logo.png" alt="" width="160px" height="69px" />
+          </Link>
           <li className={styles.listItem}>Events</li>
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
+      <Link href="/cart">
       <div className={styles.item}>
         <div className={styles.cart}>
           <Image src="/img/cart.png" alt="" width="30px" height="30px" />
-          <div className={styles.counter}>2</div>
+          <div className={styles.counter}>{quantity}</div>
         </div>
       </div>
+      </Link>
     </div>
+    
   );
 };
 
